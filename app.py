9e826da0ba4ee649998885def212ee88646c6eee
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2016-11-19 21:24:45 -0600

    Hash Set Implemented

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ app = Flask(__name__)
 #Main Stocks library
 stocks = []
 watchlist = []
+stock_set = {}
+
 date = datetime.date(2000, 1, 3)
 end = datetime.date(2017, 1, 1)
 
@@ -24,10 +26,24 @@ class Stock:
         self.last_earning = 0
         self.change = 0
 
+
+class Stock_base:
+    def __init__(self, symbol, quantity, price):
+        self.symbol = symbol
+        self.quantity = quantity
+        self.last_price = price
+        self.earnings = 0
+
+    def __eq__(self, other):
+        if self.symbol == other.symbol:
+            return True;
+
+    def __hash__(self):
+        return self.symbol.__hash__()
+
 @app.route("/")
 def index():
     return render_template('page.html', date = date, stock = stocks, watchlist= watchlist)
-
 
 
 @app.route('/stocks')
@@ -37,15 +53,29 @@ def trade():
     q = get_quotes(symbol, date, end)
     if is_symbol(symbol):
         s = Stock(symbol, quantity, date, float(q['Open']))
+        sb = Stock_base(symbol, quantity, float(q['Open']))
     if request.args['action'] == 'watchlist':
         watchlist.append(s)
     elif request.args['action'] == 'buy':
+
+        if stock_set.get(symbol):
+            stock_set[symbol].quantity += quantity
+        else:
+            stock_set[symbol] = sb
+
         for i in range(len(stocks)):
             if stocks[i].symbol==s.symbol and stocks[i].purch_date==s.purch_date: #already there for that date
                 stocks[i].quantity += s.quantity
                 break
         else:
             stocks.append(s)
+    elif request.args['action'] == 'sell':
+        for i in range(len(stocks)):
+            if stocks[i].symbol == s.symbol:
+                stocks[i].quantity += s.quantity
+
+    print(stock_set['AAPL'].last_price)
+
     return render_template('page.html', date=date, stocks=stocks, watchlist= watchlist)
 
 
@@ -57,6 +87,14 @@ def is_trading_day(date):
         return False
 
 
+def earning():
+    for s,y in stock_set.items():
+        if is_symbol(s):
+            q = get_quotes(y.symbol, date, end)
+            diff = q['Open'] - y.last_price
+            y.last_price = q['Open']
+            y.earnings += diff*y.quantity
+            print("earnings", y.earnings)
 
 def get_quotes(symbol, start_date, end_date):
     try:
@@ -77,6 +115,7 @@ def next_day():
     date += datetime.timedelta(days= 1)
     while not is_trading_day(date):
         date += datetime.timedelta(days=1)
+    earning()
     return render_template('page.html', date=date, stocks=stocks, watchlist=watchlist)
 
 def refresh_stocks():
